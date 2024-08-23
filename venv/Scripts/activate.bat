69688bd20172f878e93b77b32b7e1d8aa7b46fb0
hayyu.ragea <hayyu.ragea@gmail.com>
2024-08-23 12:52:47 +0300

    Set up GitHub Actions for CI/CD

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,11 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
+<<<<<<< HEAD
 set VIRTUAL_ENV=C:\Users\hayyu.ragea\AppData\Local\Programs\Python\Python312\venv
+=======
+set VIRTUAL_ENV=C:\Users\hayyu.ragea\AppData\Local\Programs\Python\Python312\pythonapp\venv
+>>>>>>> 915efb85 (Initial commit: Set up Python environment and folder structure)
 
 if not defined PROMPT set PROMPT=$P$G
 
